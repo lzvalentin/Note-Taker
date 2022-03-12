2c3b842e5d7784a613ba01dcbee444e8f40388ce
Lorena <lzuniga1315@gmail.com>
2022-03-12 14:46:40 -0800

    fixed server api & can delete note

--- server.js
+++ server.js
@@ -16,6 +16,57 @@ app.use(express.static('public'));
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, 'public/index.html'))
 );    
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/notes.html'))
+);
+
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    var notesArray = JSON.parse(data);
+    res.json(notesArray);
+  });
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  newNote.id = uuid.v4();
+  fs.readFile('./db/db.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    var arrayOfNoteObjects = JSON.parse(data);
+    arrayOfNoteObjects.push(newNote);
+    fs.writeFile(
+      './db/db.json',
+      JSON.stringify(arrayOfNoteObjects),
+      'utf-8',
+      function (err) {
+        if (err) throw err;
+        res.json(arrayOfNoteObjects);
+      }
+    );
+  });
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const deletedNote = req.params.id;
+  console.log(deletedNote);
+  fs.readFile('./db/db.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    var arrayOfNoteObjects = JSON.parse(data);
+    newNoteArray = arrayOfNoteObjects.filter((note) => {
+      return note.id !== deletedNote;
+    });
+    fs.writeFile(
+      './db/db.json',
+      JSON.stringify(newNoteArray),
+      'utf-8',
+      function (err) {
+        if (err) throw err;
+        res.json(newNoteArray);
+      }
+    );
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
